0d3a2fe5a88811a24102a43678c6930a5455f3d0
Phillip <phillipcodes@gmail.com>
2017-04-26 13:36:36 -0400

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,16 +1,3 @@
-// var MyController = angular.module('MyController', []);
- //var dataText = require("./hello.json");
-
-// MyController.controller('MyController', ['$scope', '$http', function($scope, $http) {
-//     $http.get('/api/dude').
-//     success(function(data) {
-//         $scope.dude = data;
-//     });
-
-// }]);
-
-// console.log(dudeObj);
-
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('GreetingController', ['$scope', '$http', 'serviceId' , function($scope, $http, serviceId) {
